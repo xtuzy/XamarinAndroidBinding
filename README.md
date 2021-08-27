9aa80c00f792b43e16f8ec493ead3c2a051975c5
xtuzh <xtuzh@ZHOUYANG>
2021-08-27 17:17:35 +0800

    Add project files.

---
+++ README.md
@@ -0,0 +1,4 @@
+# XamarinBind
+绑定一些Android库到Xamarin  
+原Android库地址  
+- [ByRecyclerView](https://github.com/youlookwhat/ByRecyclerView)
---
+++ Xamarin.ByRecyclerView/Additions/BaseRecyclerAdapter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using AndroidX.RecyclerView.Widget;
+
+namespace ME.Jingbin.Library.Adapter
+{
+    public abstract partial class BaseRecyclerAdapter
+    {
+        /*public override AndroidX.RecyclerView.Widget.RecyclerView.ViewHolder OnCreateViewHolder(global::Android.Views.ViewGroup parent, int viewType)
+        {
+            return  OnCreateViewHolder(parent, viewType);
+        }*/
+    }
+}
